b09ba057246e50b0594ce247f21c5eba9ff11a35
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-09 12:19:03 +0100

    COMMIT 9215-4

--- inc/headerbar.php
+++ inc/headerbar.php
@@ -8,43 +8,6 @@
                             </a>
                         </li>
                         <!-- END Main Sidebar Toggle Button -->
-
-                        <!-- Template Options -->
-                        <!-- Change Options functionality can be found in js/app.js - templateOptions() -->
-                        <li class="dropdown">
-                            <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="gi gi-settings"></i>
-                            </a>
-                            <ul class="dropdown-menu dropdown-custom dropdown-options">
-                                <li class="dropdown-header text-center">Header Style</li>
-                                <li>
-                                    <div class="btn-group btn-group-justified btn-group-sm">
-                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-header-default">Light</a>
-                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-header-inverse">Dark</a>
-                                    </div>
-                                </li>
-                                <li class="dropdown-header text-center">Page Style</li>
-                                <li>
-                                    <div class="btn-group btn-group-justified btn-group-sm">
-                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-main-style">Default</a>
-                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-main-style-alt">Alternative</a>
-                                    </div>
-                                </li>
-                                <li class="dropdown-header text-center">Main Layout</li>
-                                <li>
-                                    <button class="btn btn-sm btn-block btn-primary" id="options-header-top">Fixed Side/Header (Top)</button>
-                                    <button class="btn btn-sm btn-block btn-primary" id="options-header-bottom">Fixed Side/Header (Bottom)</button>
-                                </li>
-                                <li class="dropdown-header text-center">Footer</li>
-                                <li>
-                                    <div class="btn-group btn-group-justified btn-group-sm">
-                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-footer-static">Default</a>
-                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-footer-fixed">Fixed</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </li>
-                        <!-- END Template Options -->
                     </ul>
                     <!-- END Left Header Navigation -->
 
@@ -71,48 +34,13 @@
                         <!-- User Dropdown -->
                         <li class="dropdown">
                             <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="img/placeholders/avatars/avatar2.jpg" alt="avatar"> <i class="fa fa-angle-down"></i>
+                                <img src="<?php echo SITE,FOLDER,ASSETS; ?>img/placeholders/avatars/avatar2.jpg" alt="avatar"> <i class="fa fa-angle-down"></i>
                             </a>
                             <ul class="dropdown-menu dropdown-custom dropdown-menu-right">
-                                <li class="dropdown-header text-center">Account</li>
                                 <li>
-                                    <a href="page_ready_timeline.html">
-                                        <i class="fa fa-clock-o fa-fw pull-right"></i>
-                                        <span class="badge pull-right">10</span>
-                                        Updates
-                                    </a>
-                                    <a href="page_ready_inbox.html">
-                                        <i class="fa fa-envelope-o fa-fw pull-right"></i>
-                                        <span class="badge pull-right">5</span>
-                                        Messages
-                                    </a>
-                                    <a href="page_ready_pricing_tables.html"><i class="fa fa-magnet fa-fw pull-right"></i>
-                                        <span class="badge pull-right">3</span>
-                                        Subscriptions
-                                    </a>
-                                    <a href="page_ready_faq.html"><i class="fa fa-question fa-fw pull-right"></i>
-                                        <span class="badge pull-right">11</span>
-                                        FAQ
-                                    </a>
+                                    <a href="<?php echo SITE,FOLDER; ?>logout.php"><i class="fa fa-ban fa-fw pull-right"></i> Déconnexion</a>
                                 </li>
-                                <li class="divider"></li>
-                                <li>
-                                    <a href="page_ready_user_profile.html">
-                                        <i class="fa fa-user fa-fw pull-right"></i>
-                                        Profile
-                                    </a>
-                                    <!-- Opens the user settings modal that can be found at the bottom of each page (page_footer.html in PHP version) -->
-                                    <a href="#modal-user-settings" data-toggle="modal">
-                                        <i class="fa fa-cog fa-fw pull-right"></i>
-                                        Settings
-                                    </a>
-                                </li>
-                                <li class="divider"></li>
-                                <li>
-                                    <a href="page_ready_lock_screen.html"><i class="fa fa-lock fa-fw pull-right"></i> Lock Account</a>
-                                    <a href="login.html"><i class="fa fa-ban fa-fw pull-right"></i> Logout</a>
-                                </li>
-                                <li class="dropdown-header text-center">Activity</li>
+                                <!-- <li class="dropdown-header text-center">Activity</li>
                                 <li>
                                     <div class="alert alert-success alert-alt">
                                         <small>5 min ago</small><br>
@@ -130,7 +58,7 @@
                                         <small>Yesterday</small><br>
                                         <i class="fa fa-bug fa-fw"></i> <a href="javascript:void(0)" class="alert-link">New bug submitted</a>
                                     </div>
-                                </li>
+                                </li> -->
                             </ul>
                         </li>
                         <!-- END User Dropdown -->
