5bcc190d877e2fca3e1e15bf4b49f33761c67b83
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-09 17:05:29 +0100

    COMMIT 9215-27

--- inc/control/meteo-api.php
+++ inc/control/meteo-api.php
@@ -3,7 +3,7 @@
 
 //Minimum request
 //Can be city,state,country, zip/postal code, IP address, longtitude/latitude. If long/lat are 2 elements, they will be assembled. IP address is one element.
-$loc_array= Array("44000","fr");		//data validated in foreach. 
+$loc_array= Array("46.50,-1.7833");		//data validated in foreach. 
 $api_key="b04cc5e17440c7232f5e70cdf0e55";		//should be embedded in your code, so no data validation necessary, otherwise if(strlen($api_key)!=24)
 $num_of_days=2;					//data validated in sprintf
 
@@ -14,7 +14,7 @@ foreach($loc_array as $loc){
 $loc_string=implode(",", $loc_safe);
 
 //To add more conditions to the query, just lengthen the url string
-$basicurl=sprintf('http://api.worldweatheronline.com/free/v1/weather.ashx?key=%s&q=%s&num_of_days=%s', 
+$basicurl=sprintf('http://api.worldweatheronline.com/free/v2/weather.ashx?key=%s&q=%s&num_of_days=%s', 
 	$api_key, $loc_string, intval($num_of_days));
 
 print $basicurl . "<br />";
