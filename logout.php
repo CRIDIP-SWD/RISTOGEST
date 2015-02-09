ff96a156e87dd031403c8eff5a96ac96c530d544
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-09 19:04:06 +0100

    logout.php

---
+++ logout.php
@@ -0,0 +1,11 @@
+    <?php
+    session_start();
+    include ('db.conf.php');
+    $login = $_SESSION['login'];
+    BaseConnect();
+    $sql_dest = mysql_query("UPDATE salarie SET connect = '0' WHERE login = '$login'")or die(mysql_error())
+    session_unset();
+    session_destroy();
+    header('Location: index.php');
+    exit();
+    ?>
