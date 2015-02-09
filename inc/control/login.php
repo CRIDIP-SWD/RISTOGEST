a7bd52ff059f21f2e8076353dc11691e36331543
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-09 18:46:01 +0100

    COMMIT 9215-54

--- inc/control/login.php
+++ inc/control/login.php
@@ -12,24 +12,7 @@
                                     $sql = 'SELECT count(*) FROM salarie WHERE login="'.mysql_escape_string($_POST['login']).'"  AND pass_md5="'.mysql_escape_string(md5($_POST['pass'])).'"';
                                     $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
                                     $data = mysql_fetch_array($req);
-
-
-
-                                    // si on obtient une réponse, alors l'utilisateur est un membre
-                                    if ($data[0] == 1) {
-                                        session_start();
-                                        $_SESSION['login'] = $_POST['login'];
-                                        header('Location: index.php');
-                                        exit();
-                                    }
-                                    // si on ne trouve aucune réponse, le visiteur s'est trompé soit dans son login, soit dans son mot de passe
-                                    elseif ($data[0] == 0) {
-                                        header("Location: ../../login.php?alert_fail=true&error=1&error_base=false&error_champs=false");
-                                    }
-                                    // sinon, alors la, il y a un gros problème :)
-                                    else {
-                                        header("Location: ../../login.php?error_base=true&error=1&alert_fail=false&error_champs=false");
-                                    }
+                                    echo $data;
                                 }
                                 else {
                                 header("Location: ../../login.php?error_champs=true&error=1&alert_fail=false&error_base=false");
