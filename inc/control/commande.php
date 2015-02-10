622c7a08e69e8af06a6c97adf1e968561e71cfb7
cridip <mmockelyn@cridip.com>
2015-02-10 23:36:34 +0100

    COMMANDE --> CONTROL, VIEW

--- inc/control/commande.php
+++ inc/control/commande.php
@@ -12,11 +12,27 @@ if(isset($_GET['etape']) && $_GET['etape'] == 'step1')
 
 	$sql_new_commande = mysql_query("INSERT INTO `commande`(`idcommande`, `idmenu`, `idsalarie`, `date_commande`, `montant_total`, `etat_commande`) 
 		VALUES (NULL,'$idmenu','$idsalarie','$date_systeme','0','1')");
+	
+
+	$sql_commande = mysql_query("SELECT * FROM commande WHERE num_commande = '$num_commande'")or die(mysql_error());
+	$donnee_commande = mysql_fetch_array($sql_commande);
+	$idcommande = $donnee_commande['idcommande'];
+
 
 	if($sql_new_commande == TRUE){
+
+		mysql_query("INSERT INTO `log_commande`(`idlogcommande`, `idcommande`, `categorie_log`, `desc_log`, `date_log`, `etat_log`) 
+			VALUES (NULL,'$idcommande','1','Votre commande à été transmis au centre de gestion de commande.','$date_systeme - $heure_systeme','2')")or die(mysql_error());
+
 		header("Location: ../../module/commande/liste.php?idsalarie=$idsalarie&new_commande=success");
+
 	}else{
+
+		mysql_query("INSERT INTO `log_commande`(`idlogcommande`, `idcommande`, `categorie_log`, `desc_log`, `date_log`, `etat_log`) 
+			VALUES (NULL,'$idcommande','1','Une erreur à eu lieu lors de la création de votre commande.','$date_systeme - $heure_systeme','0')")or die(mysql_error());
+
 		header("Location: ../../module/commande/liste.php?idsalarie=$idsalarie&new_commande=error");
+
 	}
 }
  ?>
---
+++ module/commande/view.php
@@ -0,0 +1,194 @@
+<?php include ('../../inc/header.php'); ?>
+<?php
+$idcommande = $_GET['idcommande'];
+$sql_commande = mysql_query("SELECT * FROM commande WHERE idcommande = '$idcommande'")or die(mysql_error());
+$donnee_commande = mysql_fetch_array($sql_commande);
+?>
+<?php
+define("TITLE_PAGE", "FICHE COMMANDE".$donnee_commande['num_commande']);
+define("SUBTITLE_PAGE", "LISTE DES COMMANDES");
+//Breadcumb
+$li_start = "<li>".$logiciel."</li>";
+$li1 = "COMMANDE";
+$li2 = "";
+$li3 = "";
+$li4 = "";
+$li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
+?>
+    <body>
+        <!-- Preloader -->
+        <!-- Preloader functionality (initialized in js/app.js) - pageLoading() -->
+        <!-- Used only if page preloader is enabled from inc/config (PHP version) or the class 'page-loading' is added in body element (HTML version) -->
+        <div class="preloader themed-background">
+            <h1 class="push-top-bottom text-light text-center"><strong>Pro</strong>UI</h1>
+            <div class="inner">
+                <h3 class="text-light visible-lt-ie9 visible-lt-ie10"><strong>Loading..</strong></h3>
+                <div class="preloader-spinner hidden-lt-ie9 hidden-lt-ie10"></div>
+            </div>
+        </div>
+        <!-- END Preloader -->
+
+        <!-- Page Container -->
+        <!-- In the PHP version you can set the following options from inc/config file -->
+        <!--
+            Available #page-container classes:
+
+            '' (None)                                       for a full main and alternative sidebar hidden by default (> 991px)
+
+            'sidebar-visible-lg'                            for a full main sidebar visible by default (> 991px)
+            'sidebar-partial'                               for a partial main sidebar which opens on mouse hover, hidden by default (> 991px)
+            'sidebar-partial sidebar-visible-lg'            for a partial main sidebar which opens on mouse hover, visible by default (> 991px)
+
+            'sidebar-alt-visible-lg'                        for a full alternative sidebar visible by default (> 991px)
+            'sidebar-alt-partial'                           for a partial alternative sidebar which opens on mouse hover, hidden by default (> 991px)
+            'sidebar-alt-partial sidebar-alt-visible-lg'    for a partial alternative sidebar which opens on mouse hover, visible by default (> 991px)
+
+            'sidebar-partial sidebar-alt-partial'           for both sidebars partial which open on mouse hover, hidden by default (> 991px)
+
+            'sidebar-no-animations'                         add this as extra for disabling sidebar animations on large screens (> 991px) - Better performance with heavy pages!
+
+            'style-alt'                                     for an alternative main style (without it: the default style)
+            'footer-fixed'                                  for a fixed footer (without it: a static footer)
+
+            'disable-menu-autoscroll'                       add this to disable the main menu auto scrolling when opening a submenu
+
+            'header-fixed-top'                              has to be added only if the class 'navbar-fixed-top' was added on header.navbar
+            'header-fixed-bottom'                           has to be added only if the class 'navbar-fixed-bottom' was added on header.navbar
+        -->
+        <div id="page-container" class="sidebar-partial sidebar-visible-lg sidebar-no-animations">
+            <!-- Alternative Sidebar -->
+            <div id="sidebar-alt">
+                <!-- Wrapper for scrolling functionality -->
+                <div class="sidebar-scroll">
+                    <!-- Sidebar Content -->
+                    <div class="sidebar-content">
+                        <?php //include ('inc/chat.php'); ?>
+
+                        <?php include ('inc/activity.php'); ?>
+
+                        <?php include ('inc/message_ui.php'); ?>
+                    </div>
+                    <!-- END Sidebar Content -->
+                </div>
+                <!-- END Wrapper for scrolling functionality -->
+            </div>
+            <!-- END Alternative Sidebar -->
+
+            <?php include ('../../inc/sidebar.php'); ?>
+
+            <!-- Main Container -->
+            <div id="main-container">
+                <!-- Header -->
+                <!-- In the PHP version you can set the following options from inc/config file -->
+                <!--
+                    Available header.navbar classes:
+
+                    'navbar-default'            for the default light header
+                    'navbar-inverse'            for an alternative dark header
+
+                    'navbar-fixed-top'          for a top fixed header (fixed sidebars with scroll will be auto initialized, functionality can be found in js/app.js - handleSidebar())
+                        'header-fixed-top'      has to be added on #page-container only if the class 'navbar-fixed-top' was added
+
+                    'navbar-fixed-bottom'       for a bottom fixed header (fixed sidebars with scroll will be auto initialized, functionality can be found in js/app.js - handleSidebar()))
+                        'header-fixed-bottom'   has to be added on #page-container only if the class 'navbar-fixed-bottom' was added
+                -->
+                <?php include ('../../inc/headerbar.php'); ?>
+
+                <!-- Page content -->
+                <div id="page-content">
+                    <!-- Blank Header -->
+                    <ul class="breadcrumb breadcrumb-top">
+                        <?php
+                        if(empty($li_start)){echo "";}else{echo $li_start;}
+                        if(empty($li1)){echo "";}else{echo $li1;}
+                        if(empty($li2)){echo "";}else{echo $li2;}
+                        if(empty($li3)){echo "";}else{echo $li3;}
+                        if(empty($li4)){echo "";}else{echo $li4;}
+                        if(empty($li_end)){echo "";}else{echo $li_end;}
+                        ?>
+                    </ul>
+                    <!-- END Blank Header -->
+
+                    <!-- Example Block -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <?php
+                                if(isset($_GET['new_commande']) && $_GET['new_commande'] == 'success')
+                                {
+                            ?>
+                                <div class="alert alert-success alert-dismissable">
+                                    <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                                    <h4><i class="fa fa-check-circle"></i> Succès</h4> La commande à bien été crée !<br>
+                                    Veuillez ouvrir la commande et rentrer les produits voulus et la validée.
+                                </div>
+                            <?php } ?>
+                            <?php
+                                if(isset($_GET['new_commande']) && $_GET['new_commande'] == 'error')
+                                {
+                            ?>
+                                <div class="alert alert-danger alert-dismissable">
+                                    <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                                    <h4><i class="fa fa-times-circle"></i> Erreur</h4> Une erreur innatendue c'est produit.<br>
+                                    Information sql: <i><?php echo $_GET['error_sql']; ?></i><br>
+                                    Debug: <i><?php echo mysql_error(); ?></i><br>
+                                    <strong>Veuillez contactez l'administrateur.</strong>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+
+                    <?php
+                    if($donnee_commande['etat_commande'] == '1' AND $donnee_commande['etat_commande'] == '2')
+                    {
+                    ?>
+                    <div class="row">
+                    	<div class="col-md-3">
+                    		<div class="widget">
+								<div class="widget-extra themed-background-success">
+									<h4 class="widget-content-light"><strong><?php echo $donnee_commande['num_commande']; ?></strong></h4>
+								</div>
+								<div class="widget-extra-full">
+									<span class="h2 text-success animation-expandOpen"><?php echo $donnee_commande['date_commande']; ?></span>
+								</div>
+							</div>
+                    	</div>
+                    	<div class="col-md-3">
+                    		<div class="widget">
+								<div class="widget-extra themed-background-error">
+									<h4 class="widget-content-light"><strong>Transport</strong></h4>
+								</div>
+								<div class="widget-extra-full">
+									<span class="h2 text-success animation-expandOpen"><i class="gi gi</span>
+								</div>
+							</div>
+                    	</div>
+                    </div>
+                    <?php } ?>
+                    
+
+                    <!-- END Example Block -->
+                </div>
+                <!-- END Page Content -->
+
+                <?php include ('../../inc/footer.php'); ?>
+            </div>
+            <!-- END Main Container -->
+        </div>
+        <!-- END Page Container -->
+
+        <!-- Scroll to top link, initialized in js/app.js - scrollToTop() -->
+        <a href="#" id="to-top"><i class="fa fa-angle-double-up"></i></a>
+
+        <!-- Include Jquery library from Google's CDN but if something goes wrong get Jquery from local file (Remove 'http:' if you have SSL) -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>!window.jQuery && document.write(decodeURI('%3Cscript src="js/vendor/jquery-1.11.1.min.js"%3E%3C/script%3E'));</script>
+
+        <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
+        <script src="<?php echo SITE,FOLDER,ASSETS; ?>js/vendor/bootstrap.min.js"></script>
+        <script src="<?php echo SITE,FOLDER,ASSETS; ?>js/plugins.js"></script>
+        <script src="<?php echo SITE,FOLDER,ASSETS; ?>js/app.js"></script>
+        
+        <script src="<?php echo SITE,FOLDER,ASSETS; ?>js/pages/tablesDatatables.js"></script>
+        <script>$(function(){ TablesDatatables.init(); });</script>
+    </body>
+</html>
