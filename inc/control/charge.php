130454285e42cb158e63df2a7fb774c3b530d1a6
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-10 18:54:32 +0100

    CHARGE

--- inc/control/charge.php
+++ inc/control/charge.php
@@ -4,7 +4,7 @@
 //Vérification si le menu n'a pas dépasser la date
 $sql_menu = mysql_query("SELECT * FROM menu");
 $donnee_menu = mysql_fetch_array($sql_menu);
-if($donnee_menu['date_menu'] > $date_systeme){
+if($donnee_menu['date_menu'] < $date_systeme){
 	echo $donnee_menu['idmenu'];
 }
 ?>
