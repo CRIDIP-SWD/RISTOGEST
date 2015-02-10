cac6ee40c49e3b24a070038cd7b4c4c6468a2524
cridip <mmockelyn@cridip.com>
2015-02-11 00:57:44 +0100

    COMMANDE -> VIEW

--- module/commande/view.php
+++ module/commande/view.php
@@ -390,7 +390,7 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                             </div>
                         </div>
 
-                        <div class="col-sm-3" data-placement="top" data-toggle="tooltip" data-original-title="Votre commande est en attente de validation !">
+                        <div class="col-sm-3" data-placement="top" data-toggle="tooltip" data-original-title="Votre commande est en cours de vérification !">
                             <div class="block">
                                 <div class="block-title" style="background-color: orange; font-size: 18px; padding-top: 10px; padding-bottom: 10px;">
                                     <div style="color: white; font-weight: bold; text-align: center;"><i class="gi gi-ok_2"></i> DISPONIBLE</div>
@@ -449,6 +449,59 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                                     <div style="color: white; font-weight: bold; text-align: center;"><i class="gi gi-ok_2"></i> DISPONIBLE</div>
                                 </div>
                                 <div class="text-center text-success" style="position: relative; top: -10px; font-size: 30px; padding-top: 10px; padding-bottom: 10px;"><?php echo $donnee_commande['date_disponible']; ?></div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <?php } ?>
+
+                    <?php
+                    if($donnee_commande['etat_commande'] == '9')
+                    {
+                    ?>
+                    <div class="row">
+                        <div class="col-sm-2" data-placement="top" data-toggle="tooltip" data-original-title="Votre commande est créer et valider !">
+                            <div class="block">
+                                <div class="block-title" style="background-color: green; font-size: 18px; padding-top: 10px; padding-bottom: 10px;">
+                                    <div style="color: white; font-weight: bold; text-align: center;"><i class="fa fa-check"></i> <?php echo $donnee_commande['num_commande']; ?></div>
+                                </div>
+                                <div class="text-center text-success" style="position: relative; top: -10px; font-size: 30px; padding-top: 10px; padding-bottom: 10px;"><?php echo $donnee_commande['date_commande']; ?></div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-2" data-placement="top" data-toggle="tooltip" data-original-title="Votre commande est prise en charge par le centre de gestion !">
+                            <div class="block">
+                                <div class="block-title" style="background-color: green; font-size: 18px; padding-top: 10px; padding-bottom: 10px;">
+                                    <div style="color: white; font-weight: bold; text-align: center;"><i class="fa fa-check"></i> PRISE EN CHARGE</div>
+                                </div>
+                                <div class="text-center text-success" style="position: relative; top: -10px; font-size: 30px; padding-top: 10px; padding-bottom: 10px;"><?php echo $donnee_commande['date_pris_charge']; ?></div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-2" data-placement="top" data-toggle="tooltip" data-original-title="Votre commande est préparer par le prestataire et est envoyer !">
+                            <div class="block">
+                                <div class="block-title" style="background-color: green; font-size: 18px; padding-top: 10px; padding-bottom: 10px;">
+                                    <div style="color: white; font-weight: bold; text-align: center;"><i class="gi gi-check"></i> PRESTATAIRE</div>
+                                </div>
+                                <div class="text-center text-success" style="position: relative; top: -10px; font-size: 30px; padding-top: 10px; padding-bottom: 10px;"><?php echo $donnee_commande['date_prestataire']; ?></div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-3" data-placement="top" data-toggle="tooltip" data-original-title="Votre commande est arrivé sur son lieu de livraison !">
+                            <div class="block">
+                                <div class="block-title" style="background-color: green; font-size: 18px; padding-top: 10px; padding-bottom: 10px;">
+                                    <div style="color: white; font-weight: bold; text-align: center;"><i class="gi gi-check"></i> LIVRAISON</div>
+                                </div>
+                                <div class="text-center text-success" style="position: relative; top: -10px; font-size: 30px; padding-top: 10px; padding-bottom: 10px;"><?php echo $donnee_commande['date_livraison']; ?></div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-3" data-placement="top" data-toggle="tooltip" data-original-title="Une erreur sur votre commande est detecter !">
+                            <div class="block">
+                                <div class="block-title" style="background-color: red; font-size: 18px; padding-top: 10px; padding-bottom: 10px;">
+                                    <div style="color: white; font-weight: bold; text-align: center;"><i class="gi gi-ok_2"></i> DISPONIBLE</div>
+                                </div>
+                                <div class="text-center text-error" style="position: relative; top: -10px; font-size: 30px; padding-top: 10px; padding-bottom: 10px;"><i class="gi gi-remove_2"></i></div>
                             </div>
                         </div>
                         
