9e6b8b064dd333c5e66fe36cc00c776f8f5cedf7
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-10 15:12:43 +0100

    blank, menu->index

---
+++ module/menu/index.php
@@ -0,0 +1,242 @@
+<?php
+include ('../../inc/header.php');
+?>
+    <body>
+        <!-- Preloader -->
+        <!-- Preloader functionality (initialized in js/app.js) - pageLoading() -->
+        <!-- Used only if page preloader is enabled from inc/config (PHP version) or the class 'page-loading' is added in body element (HTML version) -->
+        <div class="preloader themed-background">
+            <h1 class="push-top-bottom text-light text-center"><strong>Pro</strong>UI</h1>
+            <div class="inner">
+                <h3 class="text-light visible-lt-ie9 visible-lt-ie10"><strong>Loading..</strong></h3>
+                <div class="preloader-spinner hidden-lt-ie9 hidden-lt-ie10"></div>
+            </div>
+        </div>
+        <!-- END Preloader -->
+
+        <!-- Page Container -->
+        <!-- In the PHP version you can set the following options from inc/config file -->
+        <!--
+            Available #page-container classes:
+
+            '' (None)                                       for a full main and alternative sidebar hidden by default (> 991px)
+
+            'sidebar-visible-lg'                            for a full main sidebar visible by default (> 991px)
+            'sidebar-partial'                               for a partial main sidebar which opens on mouse hover, hidden by default (> 991px)
+            'sidebar-partial sidebar-visible-lg'            for a partial main sidebar which opens on mouse hover, visible by default (> 991px)
+
+            'sidebar-alt-visible-lg'                        for a full alternative sidebar visible by default (> 991px)
+            'sidebar-alt-partial'                           for a partial alternative sidebar which opens on mouse hover, hidden by default (> 991px)
+            'sidebar-alt-partial sidebar-alt-visible-lg'    for a partial alternative sidebar which opens on mouse hover, visible by default (> 991px)
+
+            'sidebar-partial sidebar-alt-partial'           for both sidebars partial which open on mouse hover, hidden by default (> 991px)
+
+            'sidebar-no-animations'                         add this as extra for disabling sidebar animations on large screens (> 991px) - Better performance with heavy pages!
+
+            'style-alt'                                     for an alternative main style (without it: the default style)
+            'footer-fixed'                                  for a fixed footer (without it: a static footer)
+
+            'disable-menu-autoscroll'                       add this to disable the main menu auto scrolling when opening a submenu
+
+            'header-fixed-top'                              has to be added only if the class 'navbar-fixed-top' was added on header.navbar
+            'header-fixed-bottom'                           has to be added only if the class 'navbar-fixed-bottom' was added on header.navbar
+        -->
+        <div id="page-container" class="sidebar-partial sidebar-visible-lg sidebar-no-animations">
+            <!-- Alternative Sidebar -->
+            <div id="sidebar-alt">
+                <!-- Wrapper for scrolling functionality -->
+                <div class="sidebar-scroll">
+                    <!-- Sidebar Content -->
+                    <div class="sidebar-content">
+                        <?php //include ('inc/chat.php'); ?>
+
+                        <?php include ('../../inc/activity.php'); ?>
+
+                        <?php include ('../../inc/message_ui.php'); ?>
+                    </div>
+                    <!-- END Sidebar Content -->
+                </div>
+                <!-- END Wrapper for scrolling functionality -->
+            </div>
+            <!-- END Alternative Sidebar -->
+
+            <?php include ('../../inc/sidebar.php'); ?>
+
+            <!-- Main Container -->
+            <div id="main-container">
+                <!-- Header -->
+                <!-- In the PHP version you can set the following options from inc/config file -->
+                <!--
+                    Available header.navbar classes:
+
+                    'navbar-default'            for the default light header
+                    'navbar-inverse'            for an alternative dark header
+
+                    'navbar-fixed-top'          for a top fixed header (fixed sidebars with scroll will be auto initialized, functionality can be found in js/app.js - handleSidebar())
+                        'header-fixed-top'      has to be added on #page-container only if the class 'navbar-fixed-top' was added
+
+                    'navbar-fixed-bottom'       for a bottom fixed header (fixed sidebars with scroll will be auto initialized, functionality can be found in js/app.js - handleSidebar()))
+                        'header-fixed-bottom'   has to be added on #page-container only if the class 'navbar-fixed-bottom' was added
+                -->
+                <?php include ('../../inc/headerbar.php'); ?>
+
+                <!-- Page content -->
+                <div id="page-content">
+                    <!-- User Profile Header -->
+                    <!-- For an image header add the class 'content-header-media' and an image as in the following example -->
+                    <div class="content-header content-header-media">
+                        <div class="header-section">
+                            <img src="<?php echo SITE,FOLDER,ASSETS; ?>img/placeholders/avatars/avatar2.jpg" alt="Avatar" class="pull-right img-circle">
+                            <h1><?php echo $donnee_user['prenom']; ?> <?php echo $donnee_user['nom']; ?> <br><small>Connexion par: <?php echo $donnee_user['login']; ?></small></h1>
+                        </div>
+                        <!-- For best results use an image with a resolution of 2560x248 pixels (You can also use a blurred image with ratio 10:1 - eg: 1000x100 pixels - it will adjust and look great!) -->
+                        <img src="<?php echo SITE,FOLDER,ASSETS; ?>img/placeholders/headers/profile_header.jpg" alt="header image" class="animation-pulseSlow">
+                    </div>
+                    <!-- END User Profile Header -->
+
+                    <!-- User Profile Content -->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="block">
+                                <!-- Interactive Title -->
+                                <div class="block-title">
+                                    <!-- Interactive block controls (initialized in js/app.js -> interactiveBlocks()) -->
+                                    <div class="block-options pull-right">
+                                        <a href="#modal-user-settings" class="btn btn-alt btn-sm btn-primary" data-toggle="modal"><i class="fa fa-cog"></i></a>
+                                    </div>
+                                    <h2>Vos Informations</h2>
+                                </div>
+                                <!-- END Interactive Title -->
+
+                                <!-- Interactive Content -->
+                                <!-- The content you will put inside div.block-content, will be toggled -->
+                                <div class="block-content">
+                                    
+                                </div>
+                                <p class="text-muted">You can also have content that ignores toggling..</p>
+                                <!-- END Interactive Content -->
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="col-md-6">
+                                <div class="widget">
+                                    <div class="widget-simple">
+                                        <a class="widget-icon pull-left themed-background" href="javascript:void(0)">
+                                            <i class="gi gi-package"></i>
+                                        </a>
+                                        <h3 class="text-right animation-stretchRight">5 <strong>Menus Commander</strong></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="widget">
+                                    <div class="widget-simple">
+                                        <a class="widget-icon pull-left themed-background" href="javascript:void(0)">
+                                            <i class="gi gi-package"></i>
+                                        </a>
+                                        <h3 class="text-right animation-stretchRight">5 <strong>Menus Récéptionner</strong></h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- END User Profile Content -->
+                </div>
+                <!-- END Page Content -->
+
+                <!-- Footer -->
+                <?php include ('../../inc/footer.php'); ?>
+                <!-- END Footer -->
+            </div>
+            <!-- END Main Container -->
+        </div>
+        <!-- END Page Container -->
+
+        <!-- Scroll to top link, initialized in js/app.js - scrollToTop() -->
+        <a href="#" id="to-top"><i class="fa fa-angle-double-up"></i></a>
+
+        <!-- User Settings, modal which opens from Settings link (found in top right user menu) and the Cog link (found in sidebar user info) -->
+        <div id="modal-user-settings" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- Modal Header -->
+                    <div class="modal-header text-center">
+                        <h2 class="modal-title"><i class="fa fa-pencil"></i> Settings</h2>
+                    </div>
+                    <!-- END Modal Header -->
+
+                    <!-- Modal Body -->
+                    <div class="modal-body">
+                        <form action="index.html" method="post" enctype="multipart/form-data" class="form-horizontal form-bordered" onsubmit="return false;">
+                            <fieldset>
+                                <legend>Vital Info</legend>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label">Username</label>
+                                    <div class="col-md-8">
+                                        <p class="form-control-static">Admin</p>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="user-settings-email">Email</label>
+                                    <div class="col-md-8">
+                                        <input type="email" id="user-settings-email" name="user-settings-email" class="form-control" value="admin@example.com">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="user-settings-notifications">Email Notifications</label>
+                                    <div class="col-md-8">
+                                        <label class="switch switch-primary">
+                                            <input type="checkbox" id="user-settings-notifications" name="user-settings-notifications" value="1" checked>
+                                            <span></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </fieldset>
+                            <fieldset>
+                                <legend>Password Update</legend>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="user-settings-password">New Password</label>
+                                    <div class="col-md-8">
+                                        <input type="password" id="user-settings-password" name="user-settings-password" class="form-control" placeholder="Please choose a complex one..">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="user-settings-repassword">Confirm New Password</label>
+                                    <div class="col-md-8">
+                                        <input type="password" id="user-settings-repassword" name="user-settings-repassword" class="form-control" placeholder="..and confirm it!">
+                                    </div>
+                                </div>
+                            </fieldset>
+                            <div class="form-group form-actions">
+                                <div class="col-xs-12 text-right">
+                                    <button type="button" class="btn btn-sm btn-default" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-sm btn-primary">Save Changes</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- END Modal Body -->
+                </div>
+            </div>
+        </div>
+        <!-- END User Settings -->
+
+        <!-- Include Jquery library from Google's CDN but if something goes wrong get Jquery from local file (Remove 'http:' if you have SSL) -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>!window.jQuery && document.write(decodeURI('%3Cscript src="js/vendor/jquery-1.11.1.min.js"%3E%3C/script%3E'));</script>
+
+        <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
+        <script src="<?php echo SITE, FOLDER,ASSETS; ?>js/vendor/bootstrap.min.js"></script>
+        <script src="<?php echo SITE, FOLDER,ASSETS; ?>js/plugins.js"></script>
+        <script src="<?php echo SITE, FOLDER,ASSETS; ?>js/app.js"></script>
+
+        <!-- Google Maps API + Gmaps Plugin, must be loaded in the page you would like to use maps (Remove 'http:' if you have SSL) -->
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="<?php echo SITE, FOLDER,ASSETS; ?>js/helpers/gmaps.min.js"></script>
+
+        <!-- Load and execute javascript code used only in this page -->
+        <script src="<?php echo SITE, FOLDER,ASSETS; ?>js/pages/readyProfile.js"></script>
+        <script>$(function(){ ReadyProfile.init(); });</script>
+    </body>
+</html>
