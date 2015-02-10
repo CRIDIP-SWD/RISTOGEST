aef4cbd897b2d5e0e877a0a77a3db83d5d70d2c2
cridip <mmockelyn@cridip.com>
2015-02-10 01:38:17 +0100

    profil-->Modification

--- inc/control/profil.php
+++ inc/control/profil.php
@@ -6,6 +6,7 @@ if(isset($_POST['modif-info-profil']) && $_POST['modif-info-profil'] = 'Valider'
 	$code_postal = $_POST['code_postal'];
 	$ville = htmlentities(addslashes($_POST['ville']));
 	$telephone = $_POST['telephone'];
+	$avatar = $_FILES['avatar']['name'];
 
 	//Test avatar 
 	if(isset($_FILES['avatar']) AND $_FILES['avatar']['error'] == 0){
@@ -19,10 +20,10 @@ if(isset($_POST['modif-info-profil']) && $_POST['modif-info-profil'] = 'Valider'
 				move_uploaded_file($_FILES['avatar']['tmp_name'], SITE."".FOLDER."".ASSETS."/img/placeholders/avatar/".basename($_FILE['avatar']['name']));
 
 			}else{
-				header("Location:".SITE."".FOLDER."module/user/profil.php?idsalarie=$idsalarie&error_file_extension=true");
+				header("Location: ../../module/user/profil.php?idsalarie=$idsalarie&error_file_extension=true");
 			}
 		}else{
-			header("Location:".SITE."".FOLDER."module/user/profil.php?idsalarie=$idsalarie&error_file_size=true");
+			header("Location: ../../module/user/profil.php?idsalarie=$idsalarie&error_file_size=true");
 		}
 	}
 
@@ -33,9 +34,9 @@ if(isset($_POST['modif-info-profil']) && $_POST['modif-info-profil'] = 'Valider'
 
 	if($sql_maj_salarie == TRUE)
 	{
-		header("Location:".SITE."".FOLDER."module/user/profil.php?idsalarie=$idsalarie&succes_modif_info=true");
+		header("Location: ../../module/user/profil.php?idsalarie=$idsalarie&succes_modif_info=true");
 	}else{
-		header("Location:".SITE."".FOLDER."module/user/profil.php?idsalarie=$idsalarie&error_modif_info=true");
+		header("Location: ../../module/user/profil.php?idsalarie=$idsalarie&error_modif_info=true");
 	}
 
 
