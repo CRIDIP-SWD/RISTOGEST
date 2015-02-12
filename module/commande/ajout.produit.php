857d135c71b937f3ece0852367d4e4e67b705a1c
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-12 18:03:54 +0100

    COMMANDE --> AJOUT

--- module/commande/ajout.produit.php
+++ module/commande/ajout.produit.php
@@ -126,7 +126,7 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                         <!-- END Example Title -->
 
                         <!-- Example Content -->
-                        <form action="<?php echo SITE,FOLDER; ?>inc/control/commande.php" method="post" class="form-horizontal form-bordered" onsubmit="return false;">
+                        <form action="<?php echo SITE,FOLDER; ?>inc/control/commande.php" method="post" class="form-horizontal form-bordered">
                             <input type="hidden" name="idcommande" value="<?php echo $idcommande; ?>" />
                             <div class="form-group">
                                 <label class="col-md-3 control-label" for="example-select">Produit</label>
