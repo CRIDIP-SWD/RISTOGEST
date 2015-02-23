1400156bbc122c96d56272d60423fa182a68f05d
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-23 17:00:50 +0100

    COMMIT 23215-17

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@
     include ('inc/db.conf.php');
     $login = $_SESSION['login'];
     BaseConnect();
-    mysql_query("UPDATE salarie SET connect = '0' WHERE login = '$login'")or die(mysql_error());
+    mysql_query("UPDATE utilisateur SET connect = '0' WHERE login = '$login'")or die(mysql_error());
     session_unset();
     session_destroy();
     header('Location: index.php');
