e0460d0a2cab912d8ed55cf1001ac027c7b7a10f
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-24 16:06:18 +0100

    COMMIT 24215-42

--- module/admin/commande/user/view.php
+++ module/admin/commande/user/view.php
@@ -105,7 +105,7 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                             </h1>
                         </div>
                         <div class="pull-right">
-                            Salut
+                            <a class="btn btn-lg btn-success" href=""><i class="fa fa-truck"></i>Commande Passer chez le prestataire</a>
                         </div>
                     </div>
                     <ul class="breadcrumb breadcrumb-top">
