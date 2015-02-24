bd948b974687dc34d014193f3db87e53069897ee
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-24 18:44:34 +0100

    COMMIT 24215-58

--- module/admin/commande/presta/view.php
+++ module/admin/commande/presta/view.php
@@ -137,7 +137,7 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                                 <div class="block-title">
                                     <h2>Etat de la commande</h2>
                                 </div>
-                                <div style="text-align: center; font-size: 20px; height: 75px;">
+                                <div style="text-align: center; font-size: 20px;">
                                     <?php
                                     if($donnee_commande['etat_commande'] == "0"){
                                     ?>
