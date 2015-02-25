219d6f0791320d8db08614f27c08fba9a4d8785f
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-25 09:33:25 +0100

    COMMIT 25215-2

--- module/admin/commande/presta/view.php
+++ module/admin/commande/presta/view.php
@@ -242,7 +242,7 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                                             <select id="example-select2" name="idarticle" class="select-select2" style="width: 100%;" data-placeholder="Choose one..">
                                                 <option></option><!-- Required for data-placeholder attribute to work with Select2 plugin -->
                                                 <?php
-                                                $sql_article = mysql_query("SELECT * FROM article, famille_article WHERE article.idfamillearticle = famille_article.idfamillearticle")or die(mysql_error());
+                                                $sql_article = mysql_query("SELECT * FROM article, famille_article")or die(mysql_error());
                                                 while($donnee_article == mysql_fetch_array($sql_article))
                                                 {
                                                 ?>
