4342d4e776ba8d2d064f187698084e0be8cd3ba2
CRIDIP-SWD <mmockelyn@cridip.com>
2015-02-26 19:04:40 +0100

    COMMIT 26215-61

--- inc/control/menu.php
+++ inc/control/menu.php
@@ -23,14 +23,23 @@ if(isset($_POST['add-article']) && $_POST['add-article'] == 'Valider')
 	$idmenu = $_POST['idmenu'];
 	$idarticle = $_POST['idarticle'];
 
-	$sql_add_article_menu = mysql_query("INSERT INTO `article_menu`(`idarticlemenu`, `idmenu`, `idarticle`) 
+	//Verification que le produit ne soit pas déja insérer
+	$sql_verif_article = mysql_query("SELECT SUM(idarticle) FROM article_menu WHERE idarticle = '$idarticle'");
+	$verif_article = mysql_result($sql_verif_article, 0);
+
+	if($verif_article == 0)
+	{
+		$sql_add_article_menu = mysql_query("INSERT INTO `article_menu`(`idarticlemenu`, `idmenu`, `idarticle`) 
 		VALUES (NULL,'$idmenu','$idarticle')")or die(mysql_error());
 
-	if($sql_add_menu == TRUE)
-	{
-		header("Location: ../../module/admin/menu/view.php?idmenu=$idmenu&add-article=true");
+		if($sql_add_menu == TRUE)
+		{
+			header("Location: ../../module/admin/menu/view.php?idmenu=$idmenu&add-article=true");
+		}else{
+			header("Location: ../../module/admin/menu/view.php?idmenu=$idmenu&add-article=false");
+		}
 	}else{
-		header("Location: ../../module/admin/menu/index.php?idmenu=$idmenu&add-article=false");
+			header("Location: ../../module/admin/menu/view.php?idmenu=$idmenu&add-article=insert");
 	}
 
 }
