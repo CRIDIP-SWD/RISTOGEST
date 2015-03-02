74871ae544c83ba6cbc1d9a7faf8be596ca7842d
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-02 13:29:11 +0100

    COMMIT 2315-11

--- inc/control/charge.php
+++ inc/control/charge.php
@@ -8,7 +8,7 @@ while($donnee_menu = mysql_fetch_array($sql_menu))
 	if($donnee_menu['date_menu'] < $date_systeme)
 	{
 		$idmenu = $donnee_menu['idmenu'];
-		mysql_query("UPDATE menu SET etat_menu = '1' WHERE idmenu = '$idmenu'");
+		mysql_query("UPDATE menu SET etat_menu = '0' WHERE idmenu = '$idmenu'");
 	}
 }
 
