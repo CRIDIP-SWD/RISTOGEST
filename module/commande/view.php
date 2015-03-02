69b6f7bdb61e93794f4a4e73eed67db7aa7ec6c9
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-02 15:22:23 +0100

    COMMIT 2315-34

--- module/commande/view.php
+++ module/commande/view.php
@@ -198,7 +198,7 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                     <?php
                     $sql_commande = mysql_query("SELECT * FROM commande WHERE idcommande = '$idcommande'")or die(mysql_error());
                     $donnee_commande = mysql_fetch_array($sql_commande);
-
+                    $idmenu = $donnee_commande['idmenu'];
                     ?>
                     <!-- BLOCK -->
                     <div class="row">
