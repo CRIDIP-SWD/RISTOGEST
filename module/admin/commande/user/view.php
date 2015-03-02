21b79ab8a82e6197a00f6a6594f86678aa751e0d
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-02 15:29:20 +0100

    COMMIT 2315-24

--- module/admin/commande/user/view.php
+++ module/admin/commande/user/view.php
@@ -309,7 +309,7 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                         <div class="col-md-12">
                             <div class="block">
                                 <div class="block-title">
-                                    <h2>Produit Commander - Menu du <?php echo $donnee_commande['date_menu']; ?> / <?php echo $donnee_commande['semaine']; ?></h2>
+                                    <h2>Produit Commander - Menu du <?php echo date("d-m-Y", $donnee_commande['date_menu']); ?> / <?php echo $donnee_commande['semaine']; ?></h2>
                                     <div class="pull-right">
                                         <a href="" class="btn btn-primary"><i class="fa fa-print"></i> Imprimer la commande</a>
                                     </div>
