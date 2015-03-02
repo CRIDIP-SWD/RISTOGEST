fe4ef4cf35e3bde362721dbb95399a66346ce1bd
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-02 16:06:09 +0100

    COMMIT 2315-32

--- module/admin/etat/user/user.php
+++ module/admin/etat/user/user.php
@@ -2,6 +2,8 @@
 include('../../../../inc/config.php');
 include('../../../../inc/db.conf.php');
 BaseConnect();
+$sql_import_centre = mysql_query("SELECT * FROM setting WHERE idsetting = 1")or die(mysql_error());
+$import_centre = mysql_fetch_array($sql_import_centre);
 /**
  * HTML2PDF Librairy - example
  *
@@ -17,7 +19,19 @@ BaseConnect();
     // get the HTML
     ob_start();
 ?>
-TEST
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="../../../../inc/control/pdf/styles/pdf.css">
+</head>
+<body>
+    <div id="entete">
+        <div class="centre">
+            <div style="font-size: 30px; font-weight: bolder;"><?php echo $import_centre['raison_social']; ?></div>
+        </div>
+    </div>
+</body>
+</html>
 <?php
     $content = ob_get_clean();
 
@@ -25,7 +39,7 @@ TEST
     require_once('../../../../inc/control/pdf/html2pdf.class.php');
     try
     {
-        $html2pdf = new HTML2PDF('P', 'A4', 'fr');
+        $html2pdf = new HTML2PDF('L', 'A4', 'fr');
         $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
         $html2pdf->Output('exemple01.pdf');
     }
